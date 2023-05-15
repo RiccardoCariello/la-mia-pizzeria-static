5f5cb38c72e934e77cb28efde2a59afcd554e6c8
MillionDollarBob <riccardo.c1995@outlook.com>
2023-05-15 15:39:07 +0200

    Aggiungere i file di progetto.

---
+++ La Mia Pizzeria/La Mia Pizzeria/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using La_Mia_Pizzeria.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace La_Mia_Pizzeria.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet] //Attributo inserito all'inizio.
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
